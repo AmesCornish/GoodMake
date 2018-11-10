9b2875726ef2d74117cc84fdc8e6b01592e1e2e0
Ames <git@sys.cornishes.net>
2018-11-10 14:59:20 -0800

    Improvements to goodmake_clean.sh

--- scripts/goodmake_clean.sh
+++ scripts/goodmake_clean.sh
@@ -1,61 +1,69 @@
 #! /bin/sh -e
 
-# Clean up goodmake files
+# Clean up goodmake files.
 
-# RM="rm"
-RM="echo"  # just list deletable files
+while getopts f OPT; do
+case $OPT in
+    f) FORCE=y;;
+esac done
+shift "$((OPTIND-1))"
 
-# Don't delete anything newer than the current build
+# File list should be reviewed before deleting
+if [ -n "$FORCE" ]; then
+    RM() { echo $@; rm $@; }
+else
+    # just list deletable files
+    RM() { echo $@; }
+fi
+
+# Don't delete anything newer than the current build start
 CURRENT=$(mktemp)
 [ -z "$GM_STARTTIME" ] || touch -d "$GM_STARTTIME" "$CURRENT"
 
-find -name '.*.gm.lock' | while read REM; do
-    [ "$CURRENT" -nt "$REM" ] || continue
-    $RM "$REM"
+find "$@" -name '.*.gm.lock' | while read GM; do
+    [ "$CURRENT" -nt "$GM" ] || continue
+    RM "$GM"
 done
 
-find -name '.*.gm' | while read REM; do
-    [ "$CURRENT" -nt "$REM" ] || continue
+find "$@" -name '.*.gm' | while read GM; do
+    [ "$CURRENT" -nt "$GM" ] || continue
 
-    DIR=$(dirname $REM)
-    FILE=$(basename $REM)
+    DIR=$(dirname $GM)
+    FILE=$(basename $GM)
     FILE=${FILE%.gm}
     FILE=${FILE#.}
     TARGET="$DIR/$FILE"
 
-    BUILD_DATE=$(tail -n1 "$REM" | cut -f5)
+    BUILD_DATE=$(tail -n1 "$GM" | cut -f5)
     BUILD_DATE=$(date +%s --date "$BUILD_DATE")
-    BUILD_SUM=$(tail -n1 "$REM" | cut -f6)
+    BUILD_SUM=$(tail -n1 "$GM" | cut -f6)
 
-    REM_DATE=$(stat --format=%Y "$REM")
-
-    # tail -n1 "$REM"
+    GM_DATE=$(stat --format=%Y "$GM")
 
     if ! [ -f "$TARGET" ]; then
-        $RM "$REM"
+        RM "$GM"
         continue
     fi
 
     TGT_DATE=$(stat --format=%Y "$TARGET")
     TGT_SUM=$(md5sum "$TARGET" | cut -d' ' -f1)
 
+    # Check to make sure we're deleting a file created by GoodMake:
+
     # target mod date should be after goodmake timestamp
     #   (last line of goodmake file)
     # target mod date should be before goodmake file mod date
     # target md5 checksum should match goodmake file checksum
 
-    # echo $BUILD_DATE $TGT_DATE $REM_DATE
-    # echo $BUILD_SUM $TGT_SUM
-
-    if [ "$TGT_SUM" != "$BUILD_SUM" ]; then
-        echo "$TARGET" changed since GoodMake build
-    elif [ "$TGT_DATE" -lt "$BUILD_DATE" ]; then
-        echo "$TARGET" pre-dates GoodMake build
-    elif [ "$TGT_DATE" -gt "$REM_DATE" ]; then
-        echo "$TARGET" modified after GoodMake check
+    if [ "$TGT_DATE" -lt "$BUILD_DATE" ]; then
+        echo "$TARGET pre-dates GoodMake build" >&2
+    elif [ "$TGT_DATE" -gt "$GM_DATE" ]; then
+        echo "$TARGET modified after GoodMake check" >&2
+    elif [ "$TGT_SUM" != "$BUILD_SUM" ]; then
+        echo "$TARGET doesn't match GoodMake build" >&2
     else
-        $RM "$REM"
-        $RM "$TARGET"
+        RM "$GM"
+        RM "$TARGET"
     fi
 done
 
